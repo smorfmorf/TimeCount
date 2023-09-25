af5fc54370c789b0682e39eab99726e0660dec25
max <malalamobilelegends@gmail.com>
2023-09-26 00:24:35 +1000

    ''

--- ajax.js
+++ ajax.js
@@ -1,6 +1,6 @@
 const form = document.querySelector(".reservation__form");
 
-const URL = "https://jsonplaceholder.typicode.com/posts";
+const URL = "https://jsonplaceholder.typicode.com/posts/";
 
 function addPost(URL, { method = "POST", callback, body = {}, headers }) {
     const xhr = new XMLHttpRequest();
@@ -14,6 +14,11 @@ function addPost(URL, { method = "POST", callback, body = {}, headers }) {
     }
 
     xhr.addEventListener("load", () => {
+        if (xhr.status < 200 || xhr.status >= 300) {
+            callback(new Error("Ошибка запроса"));
+            return;
+        }
+
         const data = JSON.parse(xhr.response);
         callback(null, data);
     });
@@ -60,6 +65,9 @@ form.addEventListener("submit", (event) => {
 const form2 = document.querySelector(".footer__form");
 const input_footer = document.querySelector(".footer__input");
 
+const h2 = document.querySelector(".footer__title.footer__form-title");
+const p = document.querySelector(".footer__text");
+
 form2.addEventListener("submit", (event) => {
     event.preventDefault();
 
@@ -74,8 +82,8 @@ form2.addEventListener("submit", (event) => {
             }
 
             console.log("callback", data);
-            form2.textContent = `наши менеджеры свяжутся с вами в тчении 3-х дней`;
-            form2.style.fontSize = "12px";
+            h2.textContent = "Ваша заявка успешно отправлена";
+            p.textContent = `наши менеджеры свяжутся с вами в тчении 3-х дней`;
         },
         header: {
             "Content-Type": "application/json",
